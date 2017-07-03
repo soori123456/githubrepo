91b4ac1fc33961b0a90b8d56526843dcf8e36e21
spuerna123456789 <superna@gmail.com>
2017-07-03 10:56:23 +0530

    maven woriik

---
+++ src/main/java/com/nokia/app/App.java
@@ -0,0 +1,13 @@
+package com.nokia.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
